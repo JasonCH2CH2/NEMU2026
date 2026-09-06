bc537210cf088c9ff00145fe7c81238ac85823b6
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-06 14:38:17 +0000

    >  compile NEMU 3025244102 jason Linux 7e4580af07a3 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:38:17 up  6:58,  0 users,  load average: 0.05, 0.08, 0.04 8464a17127f8cc69d8e07b15bc8f4a77e17c479b

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,38 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(char *expr_str) {
+    if (free_ == NULL) {
+        printf("No free watchpoint.\n");
+        return NULL;
+    }
+
+    WP *wp = free_;
+    free_ = free_->next;
+
+    wp->next = head;
+    head = wp;
+
+    strncpy(wp->expr, expr_str, sizeof(wp->expr) - 1);
+    wp->expr[sizeof(wp->expr) - 1] = '\0';
+
+    bool success;
+    wp->old_value = expr(wp->expr, &success);
+
+    if (!success) {
+        printf("Bad expression.\n");
+
+        head = wp->next;
+        wp->next = free_;
+        free_ = wp;
+
+        return NULL;
+    }
+
+    printf("Watchpoint %d: %s = 0x%08x\n",
+           wp->NO, wp->expr, wp->old_value);
+
+    return wp;
+}
 
 
