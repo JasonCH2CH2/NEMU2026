f1e326ba8ffcb1dfa227e0266edb3965de40c7ee
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-06 09:10:53 +0000

    >  compile NEMU 3025244102 jason Linux 7e4580af07a3 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:10:53 up  1:31,  0 users,  load average: 0.21, 0.12, 0.06 32e9455b11c6a02935cb9ab37139d1ba92ff6ab2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -283,6 +283,16 @@ if (op == -1) {
         return !val;
     }
 
+	if (tokens[p].type == NEG) {
+        uint32_t val = eval(p + 1, q, success);
+
+    if (!*success) {
+            return 0;
+    }
+
+    return -val;
+    }
+
     *success = false;
     return 0;
 }
