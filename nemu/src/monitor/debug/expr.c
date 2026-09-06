5701bb6d0189ec6cba85043b0320278a94030451
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-06 09:12:57 +0000

    >  compile NEMU 3025244102 jason Linux 7e4580af07a3 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:12:57 up  1:33,  0 users,  load average: 0.04, 0.09, 0.06 e5fda182952bbd03a123e69f3bf7310c594add03

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -135,6 +135,20 @@ static bool make_token(char *e) {
 
 					nr_token++;
 
+					if (tokens[nr_token - 1].type == '*' &&
+					(nr_token == 1 ||
+					tokens[nr_token - 2].type == '(' ||
+					tokens[nr_token - 2].type == '+' ||
+					tokens[nr_token - 2].type == '-' ||
+					tokens[nr_token - 2].type == '*' ||
+					tokens[nr_token - 2].type == '/' ||
+					tokens[nr_token - 2].type == EQ ||
+					tokens[nr_token - 2].type == NEQ ||
+					tokens[nr_token - 2].type == AND ||
+					tokens[nr_token - 2].type == NOT)) {
+					tokens[nr_token - 1].type = DEREF;
+					}
+
 					if (tokens[nr_token - 1].type == '-' &&
 						(nr_token == 1 ||
 						tokens[nr_token - 2].type == '(' ||
