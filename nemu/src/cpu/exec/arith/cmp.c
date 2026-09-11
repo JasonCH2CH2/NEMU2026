e75ee2188322690f74085ccfccd9953408a2ffa0
zhangjunjia <wasdwasd81014@163.com>
2026-09-11 04:59:44 +0000

    PA2:cmp

---
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -0,0 +1,28 @@
+#include "cpu/exec/helper.h"
+
+make_helper(cmp_r2rm_l) {
+    int len = decode_rm_l(eip + 1);
+
+    uint32_t result = op_dest->val - reg_l(op_src->reg);
+    update_eflags_pf_zf_sf(result);
+
+    cpu.eflags.CF = op_dest->val < reg_l(op_src->reg);
+    cpu.eflags.OF = ((int32_t)op_dest->val < 0) != ((int32_t)reg_l(op_src->reg) < 0)
+                  && ((int32_t)result < 0) != ((int32_t)op_dest->val < 0);
+
+    return len + 1;
+}
+
+make_helper(cmp_i2rm_l) {
+    int len = decode_rm_l(eip + 1);
+
+    uint32_t imm = instr_fetch(eip + 1 + len, 4);
+    uint32_t result = op_dest->val - imm;
+    update_eflags_pf_zf_sf(result);
+
+    cpu.eflags.CF = op_dest->val < imm;
+    cpu.eflags.OF = ((int32_t)op_dest->val < 0) != ((int32_t)imm < 0)
+                  && ((int32_t)result < 0) != ((int32_t)op_dest->val < 0);
+
+    return len + 5;
+}
---
+++ nemu/src/cpu/exec/arith/cmp.h
@@ -0,0 +1,7 @@
+#ifndef __CMP_H__
+#define __CMP_H__
+
+make_helper(cmp_r2rm_l);
+make_helper(cmp_i2rm_l);
+
+#endif
