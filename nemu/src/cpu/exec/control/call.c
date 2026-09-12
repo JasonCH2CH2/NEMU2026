777b16831e60fea530d9e08b8729b2082b366ca5
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-12 07:53:37 +0000

    >  compile NEMU 3025244102 jason Linux 7e4580af07a3 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:53:37 up 2 days, 19 min,  0 users,  load average: 0.02, 0.05, 0.01 99db5a7596ce2611251825a5a567595ead0a3c23

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -6,3 +6,11 @@ make_helper(call_si_l) {
         cpu.eip += (int32_t)instr_fetch(eip + 1, 4);
         return 5;
 }
+
+make_helper(call_rm_l) {
+        int len = decode_rm_l(eip + 1);
+        cpu.esp -= 4;
+        swaddr_write(cpu.esp, 4, eip + len + 1);
+        cpu.eip = op_src->val - (len + 1);
+        return len + 1;
+}
