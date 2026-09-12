f0441af3f012369755fa344bc6fca02393e43e1e
tracer-NEMU2020 <tjuics2020@163.com>
2026-09-12 08:14:39 +0000

    >  compile NEMU 3025244102 jason Linux 7e4580af07a3 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  08:14:39 up 2 days, 40 min,  0 users,  load average: 0.85, 0.52, 0.31 7f8ef8bc274463f0617736c6553621864d8a35

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -69,7 +69,7 @@ make_group(group3_b,
 
 /* 0xf7 */
 make_group(group3_v,
-    test_i2rm_l, inv, not_rm_v, neg_rm_v, 
+    test_i2rm_v, inv, not_rm_v, neg_rm_v, 
     mul_rm_v, imul_rm2a_v, div_rm_v, idiv_rm_v)
 
 /* 0xfe */
@@ -127,7 +127,7 @@ helper_fun opcode_table [256] = {
 /* 0x78 */	jcc_b, jcc_b, jcc_b, jcc_b,
 /* 0x7c */	jcc_b, jcc_b, jcc_b, jcc_b,
 /* 0x80 */	group1_b, group1_v, inv, group1_sx_v, 
-/* 0x84 */  test_r2rm_b, test_r2rm_l, inv, inv,
+/* 0x84 */  test_r2rm_b, test_r2rm_v, inv, inv,
 /* 0x88 */	mov_r2rm_b, mov_r2rm_v, mov_rm2r_b, mov_rm2r_v,
 /* 0x8c */	inv, lea, inv, inv,
 /* 0x90 */	nop, inv, inv, inv,
--- nemu/src/cpu/exec/logic/test.c
+++ nemu/src/cpu/exec/logic/test.c
@@ -6,8 +6,19 @@ make_helper(test_r2rm_l) {
     uint32_t result = op_dest->val & op_src->val;
     update_eflags_pf_zf_sf(result);
 
+	return len + 1;
+}
+
+make_helper(test_r2rm_w) {
+    int len = decode_r2rm_w(eip + 1);
+
+    uint32_t result = op_dest->val & op_src->val;
+    update_eflags_pf_zf_sf(result);
+
     return len + 1;
 }
+
+make_helper_v(test_r2rm)
 
 make_helper(test_r2rm_b) {
     int len = decode_r2rm_b(eip + 1);
@@ -24,8 +35,19 @@ make_helper(test_i2rm_l) {
     uint32_t result = op_dest->val & op_src->val;
     update_eflags_pf_zf_sf(result);
 
+	return len + 1;
+}
+
+make_helper(test_i2rm_w) {
+    int len = decode_i2rm_w(eip + 1);
+
+    uint32_t result = op_dest->val & op_src->val;
+    update_eflags_pf_zf_sf(result);
+
     return len + 1;
 }
+
+make_helper_v(test_i2rm)
 
 make_helper(test_i2rm_b) {
     int len = decode_i2rm_b(eip + 1);
